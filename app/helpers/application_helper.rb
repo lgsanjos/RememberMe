d2af17c4005eb41b7a7cc409d1b3ab38290c39da
guilherme <guilherme.sanjos@gmail.com>
2011-02-10 20:28:07 -0200

    bugfixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+
+    def url_escape(string)
+      string.gsub(/([^ a-zA-Z0-9_.-]+)/n) do
+        '%' + $1.unpack('H2' * $1.size).join('%').upcase
+      end.tr(' ', '+')
+    end
+
+    def url_unescape(string)
+      string.tr('+', ' ').gsub(/((?:%[0-9a-fA-F]{2})+)/n) do
+      [$1.delete('%')].pack('H*')
+    end
+  end
+
+  def trata_msg(str)
+    str.gsub("\n", "").gsub("'","\'").gsub('"','\"')
+  end
+
 end
