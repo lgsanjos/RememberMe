f6f251b072e7dc46176155c5b42e569b5aa36cda
SAnjos <guilherme.sanjos@gmail.com>
2011-12-07 00:16:42 -0200

    Adicionado form de cadastro de desk

--- db/migrate/20110729223533_script_cria_desk_e_relaciona_notes.rb
+++ db/migrate/20110729223533_script_cria_desk_e_relaciona_notes.rb
@@ -2,23 +2,23 @@ class ScriptCriaDeskERelacionaNotes < ActiveRecord::Migration
   def self.up
     Usuario.all.each do | user |
       if user.desk.empty?
-        say "Criada desk para usuario " + user.nome
+        say "Criando desk para usuario " + user.nome
 
         desk = Desk.new
-        desk.nome = "desk"
+        desk.nome = "home"
         desk.imgicon = "home"
         desk.imgbackground = "wood_default"
         desk.private = true
         desk.shared = false
         desk.usuario_id = user.id
-        desk.ordem = 1
+        desk.ordem = 0
         desk.save        
         say "desk criada"
 
-        @notes = Note.find(:all, :conditions => {:user_id => user.id})
-        say "Há " + @notes.count.to_s + " notes do usuario para associar a desk"
+        notes = Note.find(:all, :conditions => {:user_id => user.id})
+        say "Há " + notes.count.to_s + " notes do usuario para associar a desk"
 
-        @notes.each do | note |
+        notes.each do | note |
             say "Associando Note id=" + note.id.to_s + " com a desk id=" + desk.id.to_s
             note.desk = desk
             note.save
--- public/javascripts/office/regras_interface/index.js
+++ public/javascripts/office/regras_interface/index.js
@@ -63,3 +63,149 @@ function setContextMenu(){
 }
 
 // -------------
+
+function removeNote(id) {
+
+    $.ajax({
+             type: 'POST',
+             async: true,
+             data: ({
+                'note':{
+                         'id': id
+                       }
+                     }),
+
+             url: '/notes/remove',
+
+             success: function(data){
+                        if (data === 'Ok'){
+                           $('#tr_' + id).fadeOut("fast",function(){
+                             $('#trashListTable').dataTable().fnDestroy();
+                             $(this).remove();
+                             drawTrashListTable();
+                           });
+
+
+                        }
+             },
+
+             error: function(XMLHttpRequest, textStatus, errorThrow){
+                   raiseException('Oops, an error occurred while we were removing your note: ' + errorThrow);
+             }
+
+    });
+  }
+
+//-------------
+
+  function recycleNote(id){
+    $.ajax({
+             type: 'POST',
+             async: true,
+             data: ({
+                'note':{
+                         'id': id
+                       }
+                     }),
+
+             url: '/notes/reclyce',
+
+             success: function(data){
+
+                        $('#tr_' + id).fadeOut("fast",function(){
+                           var func = '';
+                           func = 'try{';
+                           func += data;
+                           func += '}catch(e){';
+                           func += '  raiseException(e);';
+                           func += '}';
+                           eval(func);
+
+                           $('#trashListTable').dataTable().fnDestroy();
+                           $(this).remove();
+                           drawTrashListTable();
+
+                        });
+
+             },
+
+             error: function(XMLHttpRequest, textStatus, errorThrow){
+                   raiseException('Just happened an error while we were recycling your note: ' + errorThrow);
+             }
+
+    });
+  }
+  
+//-------------  
+
+  function drawTrashListTable(){
+       $('#trashListTable').dataTable({
+           "bPaginate": true,
+           "sPaginationType": "full_numbers",
+           "bLengthChange": false,
+           "bFilter": false,
+           "bSort": true,
+           "bInfo": false,
+           "bAutoWidth": false,
+           "iDisplayLength": 15,
+           "oLanguage": {
+                   "sLengthMenu": "Display _MENU_ records per page",
+                   "sZeroRecords": "Trash is empty",
+                   "sInfo": "Showing _START_ to _END_ of _TOTAL_notes",
+                   "sInfoEmpty": "showing 0 to 0 of 0 notes",
+                   "sInfoFiltered": "(filtered from _MAX_ total records)"
+           }
+       });
+  }
+
+// -------------
+  
+  function preparaSignOut() {
+	     $("#signOut").click(function() {
+	        saveChangedItensList();
+	     });
+  }
+  
+//-------------  
+  
+  function preparaPiroboxTrash() {
+
+     function onShow() {
+         $.ajax({
+                  type: 'GET',
+                  url: 'note/view_trash',
+                  async: true,
+                  success: function(data){
+
+                    $('#trashListTable').html(data);
+                    drawTrashListTable();
+
+                  },
+                  error: function(XMLHttpRequest, textStatus, errorThrow){
+                        raiseException('Sorry, but occurred an error while opening the trash: ' + errorThrow);
+
+                  }
+                });
+	 }
+
+	 try{
+	   setPirobox(htmlContentTrash, onShow);
+	 }catch(e){
+	   raiseException(e);
+	 }
+}
+  
+//-------------  
+  
+  function preparaPiroboxNewDesk() {
+
+     function onShow() {
+       $('#desk_nome').focus();
+	 }
+
+	 try{
+	   setPirobox(htmlContentNewDesk, onShow);
+	 }catch(e){
+	   raiseException(e);
+	 }
+}  
